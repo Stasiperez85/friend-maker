86ab089caa2efe01452245687f46ee06e6210639
Anastasia Perez <Stasiperez85@gmail.com>
2022-04-14 18:38:43 -0600

    add user models and crud

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    usename: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: 'Password is required',
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+    },
+    thoughts: [],
+    friends: []
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
