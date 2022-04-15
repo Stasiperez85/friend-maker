c993bd8c2f500a4ac5d609634ab99f4d72ded4ec
Anastasia Perez <Stasiperez85@gmail.com>
2022-04-14 19:17:07 -0600

    test user routes insomnia

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-    usename: {
+    username: {
         type: String,
         unique: true,
         required: 'Username is required',
